268dd158a9ed67773ca69113bd9bcb0f7c9b58e3
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-09-23 12:28:40 +0530

    Update TransformPage.tsx

--- src/pages/TransformPage.tsx
+++ src/pages/TransformPage.tsx
@@ -1,5 +1,14 @@
+import Navbar from '../Components/Navbar.tsx';
+
 const TransformPage = () => {
-  return <>Transform</>;
+  return (
+    <div className="min-h-screen bg-neutral-900 text-white">
+      <Navbar />
+      <div className="container mx-auto px-4 py-8">
+        <h1 className="mb-6 text-3xl font-bold">Transform Image</h1>
+      </div>
+    </div>
+  );
 };
 
 export default TransformPage;
