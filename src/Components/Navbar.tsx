653df552118dd49232bb0bfc522aeb49d2a80ba3
RISHABH MISHRA <rish.mish.2607@gmail.com>
2024-09-25 04:39:42 +0530

    Update Navbar.tsx

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaUserCircle } from 'react-icons/fa';
+import { FaImage, FaUserCircle } from 'react-icons/fa';
 import { Link, useLocation } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth.tsx';
 
@@ -7,57 +7,62 @@ const Navbar: React.FC = () => {
   const { logout } = useAuth();
   const location = useLocation();
   const isActive = (path: string) => {
-    return location.pathname === path ? 'text-purple-500' : 'text-white';
+    return location.pathname === path
+      ? 'text-purple-500 underline'
+      : 'text-white';
   };
 
   return (
-    <nav className="mb-4 flex items-center justify-between bg-neutral-800 p-4">
-      <Link
-        className="text-xl font-bold text-white transition duration-300 hover:text-purple-500"
-        to="/"
-      >
-        Imagery
-      </Link>
-      <div className="flex items-center space-x-4">
+    <nav className="bg-neutral-900 p-4 shadow-lg">
+      <div className="container mx-auto flex items-center justify-between">
         <Link
-          className={`${isActive('/transform')} transition duration-300 hover:text-purple-500`}
-          to="/transform"
+          className="flex items-center space-x-2 text-xl font-bold text-white transition duration-300 hover:text-purple-500"
+          to="/"
         >
-          Transform
+          <FaImage className="text-purple-500" />
+          <span>Imagery</span>
         </Link>
-        <Link
-          className={` ${isActive('/convert')} transition duration-300 hover:text-purple-500`}
-          to="/convert"
-        >
-          Convert
-        </Link>
-        <div className="relative">
-          <button
-            className="rounded-full bg-neutral-700 p-2 hover:bg-neutral-600"
-            onClick={() =>
-              document
-                .getElementById('profileDropdown')
-                ?.classList.toggle('hidden')
-            }
+        <div className="flex items-center space-x-4">
+          <Link
+            className={`${isActive('/transform')} transition duration-300 hover:text-purple-500`}
+            to="/transform"
           >
-            <FaUserCircle className="h-6 w-6" />
-          </button>
-          <div
-            className="absolute right-0 mt-2 hidden w-48 rounded-md bg-neutral-800 shadow-lg"
-            id="profileDropdown"
+            Transform
+          </Link>
+          <Link
+            className={`${isActive('/convert')} transition duration-300 hover:text-purple-500`}
+            to="/convert"
           >
-            <Link
-              className="block px-4 py-2 text-sm text-white hover:bg-neutral-700"
-              to="/profile"
-            >
-              Profile
-            </Link>
+            Convert
+          </Link>
+          <div className="relative">
             <button
-              className="block w-full px-4 py-2 text-left text-sm text-white hover:bg-neutral-700"
-              onClick={logout}
+              className="rounded-full bg-neutral-700 p-2 hover:bg-neutral-600"
+              onClick={() =>
+                document
+                  .getElementById('profileDropdown')
+                  ?.classList.toggle('hidden')
+              }
             >
-              Logout
+              <FaUserCircle className="h-6 w-6 text-white" />
             </button>
+            <div
+              className="absolute right-0 mt-2 hidden w-48 rounded-md bg-neutral-800 shadow-lg"
+              id="profileDropdown"
+            >
+              <Link
+                className={`block px-4 py-2 text-sm ${isActive('/profile')} hover:bg-neutral-700`}
+                to="/profile"
+              >
+                Profile
+              </Link>
+              <button
+                className="block w-full px-4 py-2 text-left text-sm text-white hover:bg-neutral-700"
+                onClick={logout}
+              >
+                Logout
+              </button>
+            </div>
           </div>
         </div>
       </div>
